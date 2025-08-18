fb3d129adb539d8167f82c12b8ba8ede36adb887
Pichaku123 <pribisht785@gmail.com>
2025-08-18 15:31:12 +0530

    minor bug fixes+printBoard

--- src/cell.js
+++ src/cell.js
@@ -1,7 +1,9 @@
 class Cell {
     constructor() {
-        this.occupied = null;   //changed it, now it stores object instead of t/f, i can track which ship occupies this.
+        this.occupied = null; // changed it, now it stores object instead of t/f, i can track which ship occupies this.
         this.hit = false;
+        // also, if cell is not occupied but got hit, then shot missed.
+        // (!occupied && hit)
     }
 }
 
--- src/gameboard.js
+++ src/gameboard.js
@@ -52,7 +52,7 @@ class Gameboard {
         const ship = new Ship(length);
         if (direction === "hori") {
             for (let i = 0; i < length; i++) {
-                this.board[row][col + i].occupied = ship; // each cell here points to ship.
+                this.board[row][col + i].occupied = ship; // each cell here points to ship, no need to make seperate stuff to track it.
             }
         } else if (direction === "vert") {
             for (let i = 0; i < length; i++) {
@@ -65,6 +65,25 @@ class Gameboard {
     receiveAttack(x, y) {
         //ill just update the cell's properties to mark as occupied, not sure what to do about ship tho, idk how to track ship.
     }
+
+    printBoard(){
+        for (let i = 0; i < this.size; i++) {
+            let rowOP = "";
+            for (let j = 0; j < this.size; j++) {
+                if (!gb.board[i][j].occupied && !gb.board[i][j].hit) {
+                    rowOP += ".";
+                } else if (gb.board[i][j].occupied && !gb.board[i][j].hit) {
+                    rowOP += "x";
+                }
+            }
+            console.log(`${rowOP}\n`);
+        }
+    }
 }
+
+const gb = new Gameboard(5);
+gb.placeShip(2, 1, "hori", 2);
+gb.placeShip(0, 4, "vert", 4);
+gb.printBoard();
 
 module.exports = Gameboard;
