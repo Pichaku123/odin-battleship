e3cc1c038c47611bb95f55dc6992e22d9a78c6df
Pichaku123 <pribisht785@gmail.com>
2025-08-23 12:11:28 +0530

    worked on gamecontroller logic, made turn and start logic

--- src/gamecontroller.js
+++ src/gamecontroller.js
@@ -5,7 +5,46 @@ class GameController{
     constructor(p1, p2){
         this.p1 = p1;
         this.p2 = p2;
+        this.currPlayer = p1;     //by default, we'll swap this later.
     }
-    // randomly placing ships for testing
     
+    start(){
+        //place ships before moving on.
+        this.p1.gameboard.placeShip(0, 0, "hori", 2);   //use input from ui later.
+        this.p2.gameboard.placeShip(1, 1, "hori", 2);
+        this.p2.gameboard.placeShip(2, 4, "vert", 3);
+
+        //render boards here, do it later in display.js
+        // this.p1.gameboard.printBoard();
+        // console.log("\n");
+        // this.p2.gameboard.printBoard();
+    }
+
+    turn(){
+        //assume we have coordinates from player.
+        //we play the move and see outcome.
+        //again, we track the currentPlayer throughout.
+        const oppPlayer = (this.currPlayer === this.p1) ? (this.p2) : (this.p1);
+        //now take coords and attack oppPlayer's board.
+        let tempR = 2, tempC = 3;
+        const status = oppPlayer.gameboard.receiveAttack(tempR, tempC);
+        console.log(status);
+
+        //well status part works ig, use that to do smth in ui, ofc that's tbd later.
+        //call smth from ui here
+
+        //swap current player only if they've played valid move
+        this.currPlayer = this.currPlayer === this.p1 ? this.p2 : this.p1;
+
+        //check win condition
+        if(oppPlayer.gameboard.allShipsSunk()){
+            //we win
+        }
+        else{
+            //continue, return the info about this turn for ui.
+        }
+    }
+
 }
+
+module.exports = GameController;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ const Player = require("./player");
 const Gameboard = require("./gameboard");
 const Ship = require("./ship");
 const Cell = require("./cell");
+const GameController = require("./gamecontroller");
 
 const human = new Player("human");
 const comp = new Player("comp");
 
-human.gameboard.placeShip(0, 0, "hori", 2);
-comp.gameboard.placeShip(1, 1, "hori", 2);
-
-human.gameboard.printBoard();
-console.log("\n");
-comp.gameboard.printBoard();
+const game = new GameController(human, comp);
+game.start();
