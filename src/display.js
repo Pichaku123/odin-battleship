3fd9bbfec00a749dc9c57ebbd1ed6de4f95f746f
Pichaku123 <pribisht785@gmail.com>
2025-08-24 15:47:58 +0530

    added html stuff and logic for color/display

--- src/display.js
+++ src/display.js
@@ -10,21 +10,44 @@ const UI = (gameController) => {
     const renderBoard = (currBoard) => {
         // now we've passed the gamecontroller with all the methods and stuff into UI.
         // this includes the actual boards, so we can access those too.
+
         const boardNo = currBoard === board1 ? 1 : 2;
         const boardDisplay = document.createElement("div");
         boardDisplay.classList.add(`board-${boardNo}`);
         boardContainer.appendChild(boardDisplay);
+
         for (let i = 0; i < currBoard.size; i++) {
             const row = document.createElement("div");
             row.classList.add("board-row");
+
             for (let j = 0; j < currBoard.size; j++) {
                 const cell = document.createElement("div");
                 cell.classList.add("board-cell");
                 row.appendChild(cell);
+                markCondition(currBoard.board[i][j], cell);
+                console.log(cell.classList.entries);
             }
+
             boardDisplay.appendChild(row);
         }
+
     };
+
+    const markCondition = (currCell, cellDOM) => {
+        if(currCell.occupied && currCell.hit){
+            cellDOM.classList.add("hit");
+        }
+        else if (currCell.occupied && !currCell.hit) {
+            cellDOM.classList.add("not-yet");
+        }
+        else if (!currCell.occupied && currCell.hit) {
+            cellDOM.classList.add("miss");
+        }
+        else{
+            cellDOM.classList.add("nothing");
+        }
+    }
+
     renderBoard(board1);
     renderBoard(board2);
     
