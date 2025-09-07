97418c7470b9b9683737ac0172ee6e9bd5a4628d
Pichaku123 <pribisht785@gmail.com>
2025-09-07 11:39:30 +0530

    mainly design+polishing left

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import "./styles.css";
 
-const Player = require("./player");
-const GameController = require("./gamecontroller");
 const UI = require("./display");
 
-const human = new Player("human");
-const comp = new Player("comp");
-
-const game = new GameController(human, comp);
-game.start();
 document.addEventListener("DOMContentLoaded", () => {
-    UI(game);
+    UI();
 });
