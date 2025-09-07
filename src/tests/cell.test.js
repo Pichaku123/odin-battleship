76b158a35d789fb1be4f38ad8a1e00bd12568c5e
Pichaku123 <pribisht785@gmail.com>
2025-09-07 11:52:53 +0530

    deploying incomplete version for now

--- src/tests/cell.test.js
+++
@@ -1,9 +0,0 @@
-/*eslint-disable*/
-const Cell = require("../cell");
-
-test("cell creation", () => {
-    const temp = new Cell();
-    temp.hit = true;
-    expect(temp.hit).toBe(true);
-    expect(temp.occupied).toBe(null);
-});
